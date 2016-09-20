31259a7901ee19e17601ef13f908b46d7c51b8a1
Craig Iturbe <craigiturbe@Craigs-Air.home>
2016-09-19 20:33:07 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz "
+
 def alphabetize(arr)
-  # code here
+#sorted_array = arr.sort_by do |phrase|
+=begin
+
+	phrase.split(" ").map do |word|
+		word.each_char do |letter|
+			ESPERANTO_ALPHABET.index(letter)
+		end
+
+
+	end
+end
+end=
+sorted_array
+
+=end
+
+arr.sort_by {|string| string.chars.map{|c| ESPERANTO_ALPHABET.index(c)}}
+
 end
